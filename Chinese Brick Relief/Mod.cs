47a6c69b1b24fb73138b9990999fc46390d2f0f8
kelvinmenumiz <kelvinspaces@gmail.com>
2025-02-14 03:56:34 +0800

    project init

---
+++ Chinese Brick Relief/Mod.cs
@@ -0,0 +1,36 @@
+﻿using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+using System.IO;
+using Extra.Lib.Localization;
+using System.Reflection;
+using Extra.Lib.Debugger;
+
+namespace ChineseBrickRelief
+{
+    public class Mod : IMod
+    {
+        private static readonly ILog log = LogManager.GetLogger($"{nameof(ChineseBrickRelief)}").SetShowsErrorsInUI(false);
+        internal static Logger Logger = new(log, false);
+
+        private string pathToModFolder;
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            if (!GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset)) return;
+
+            pathToModFolder = $"{new FileInfo(asset.path).DirectoryName}";
+
+            ExtraAssetsImporter.EAI.LoadCustomAssets(pathToModFolder);
+
+            ExtraLocalization.LoadLocalization(Logger, Assembly.GetExecutingAssembly());
+
+        }
+
+        public void OnDispose()
+        {
+            ExtraAssetsImporter.EAI.UnLoadCustomAssets(pathToModFolder);
+        }
+    }
+}
